4a83e91ab32e7eb1daa79aa5cf2aaf2db48621bc
Paul Muresan <paul.muresan1993rgmail.com>
2020-09-18 16:10:34 +0200

    first commit

---
+++ halconViajesWeb.js
@@ -0,0 +1,16 @@
+function recogerDatos() {
+    let caja1 = document.getElementById("Origen").value
+
+    console.log(caja1)
+
+}
+
+
+
+function datosDestino(){
+    var caja2 = document.getElementById("Destino").value
+
+    console.log(caja2)
+}
+
+
