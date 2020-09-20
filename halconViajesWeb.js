2f2145afdf38beace1c3a427b342c325557b75df
Paul271993 <paul.muresan1993@gmail.com>
2020-09-20 18:56:29 +0200

    asfas

--- halconViajesWeb.js
+++ halconViajesWeb.js
@@ -1,16 +1,66 @@
-function recogerDatos() {
-    let caja1 = document.getElementById("Origen").value
 
-    console.log(caja1)
+// document.addEventListener("DOMContentLoaded", function() {
 
-}
+//     var formulario = document.getElementById('formulario');
+//     formulario.onsubmit = function(e) {
+//       e.preventDefault();
+//       var formato = new FormData(this);
+//       var formatoJson = {};
+//       for (var [k, v] of formato) {
+//         formatoJson[k] = v;
+
+        
+        
+//       }
+      
+//       console.log(formatoJson);
+
+//     }
+  
+//   });
+
+
+  var formulariosInfo =[
+    {"origen": "Madrid",
+     "pasajeros": 1,
+      "destino": "Rumania"
+    },
+    {"origen": "Barcelona",
+     "pasajeros": 2,
+      "destino": "Mexico"
+    },
+    {"origen": "Ruanda",
+     "pasajeros": 3,
+      "destino": "EE,UU"
+    }
+
+  ];
+console.log(formulariosInfo)
+
+
+  
+  
 
 
 
-function datosDestino(){
-    var caja2 = document.getElementById("Destino").value
+//   var origen,pasajeros,destino;
 
-    console.log(caja2)
-}
+//   function guardarDatos(){
+//       origen = document.getElementById("origen").value;
+//       pasjeros = document.getElementById("pasajeros").value;
+//       destino = document.getElementById("destino").value;
 
+//       var arr = [origen,pasajeros,destino];
+//       return arr;
+//   }
 
+//   var input = document.getElementsByTagName("INPUT");
+
+//   for (i=0; i<input.length; i++) {
+//       input[i].addEventListener("change", function(){
+//           resultado = guardarDatos();
+//           console.log(resultado);
+//       });
+//   };
+
+  
